b84619bc2ce6f25634aa20a1adb224e6a1557f2f
Pranav Mur <broguyperson@gmail.com>
2026-01-23 16:58:49 -0500

    Added lights

---
+++ Assets/Scripts/lights.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class lights : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    
+    Random random = new Random();
+    int randomNumber = random.Next(1, 6);
+    
+    
+    void Start()
+    {
+   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
