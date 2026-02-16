3ddb1e284dd49dd9b96df4a2065b997b35a152e8
Pranav Mur <broguyperson@gmail.com>
2026-02-16 11:35:05 -0500

    Level Updates

--- Assets/Scripts/gameManager_script.cs
+++ Assets/Scripts/gameManager_script.cs
@@ -1,16 +1,139 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+using TMPro; 
 
-public class gameManager_script : MonoBehaviour
+public class GameManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public Lights[] lights;
+    public Button restartButton;
+
+    public float flashDuration = 0.4f;
+    public float delayBetweenFlashes = 0.2f;
+
+    List<int> pattern = new List<int>();
+    int playerIndex = 0;
+
+    bool inputEnabled;
+
+    public AudioClip yellowFlashSound;
+    public AudioClip redFlashSound;
+    public AudioClip correctClickSound;
+    public AudioClip sequenceCompleteSound;
+
+    private AudioSource audioSource;
+
+    public TMP_Text statusText; 
+
     void Start()
     {
-        
+        audioSource = GetComponent<AudioSource>();
+
+        if (restartButton != null)
+            restartButton.gameObject.SetActive(false);
+
+        StartNewRound();
     }
 
-    // Update is called once per frame
-    void Update()
+
+    void StartNewRound()
     {
-        
+        statusText.text = "";
+
+        pattern.Clear();
+        playerIndex = 0;
+        inputEnabled = false;
+
+        for (int i = 0; i < 5; i++)
+        {
+            pattern.Add(Random.Range(0, lights.Length));
+        }
+
+        StartCoroutine(PlayPattern());
+    }
+
+    IEnumerator PlayPattern()
+    {
+        yield return new WaitForSeconds(0.5f);
+
+        for (int i = 0; i < pattern.Count; i++)
+        {
+            audioSource.PlayOneShot(yellowFlashSound);
+
+            yield return StartCoroutine(
+                lights[pattern[i]].Flash(flashDuration)
+            );
+
+
+            yield return new WaitForSeconds(delayBetweenFlashes);
+        }
+
+        inputEnabled = true;
+        Debug.Log("PLAYER TURN");
+    }
+
+    public void RegisterPlayerInput(int index)
+    {
+        if (!inputEnabled) return;
+
+        if (index == pattern[playerIndex])
+        {
+            Debug.Log("CORRECT");
+            audioSource.PlayOneShot(correctClickSound);
+            playerIndex++;
+
+            if (playerIndex >= pattern.Count)
+            {
+                inputEnabled = false;
+                StartCoroutine(LevelComplete());
+            }
+
+        }
+        else
+        {
+            Debug.Log("WRONG");
+            inputEnabled = false;
+            StartCoroutine(GameOver());
+        }
+    }
+
+    IEnumerator GameOver()
+    {
+        inputEnabled = false;
+
+        statusText.text = "WRONG";
+
+        audioSource.PlayOneShot(redFlashSound);
+
+        foreach (Lights l in lights)
+        {
+            StartCoroutine(l.FlashRed(0.5f));
+        }
+
+        yield return new WaitForSeconds(0.6f);
+
+        restartButton.gameObject.SetActive(true);
+    }
+
+    IEnumerator LevelComplete()
+    {
+        inputEnabled = false;
+
+        statusText.text = "LEVEL COMPLETE";
+        audioSource.PlayOneShot(sequenceCompleteSound);
+
+        yield return new WaitForSeconds(1f);
+
+        restartButton.gameObject.SetActive(true);
+    }
+
+
+    public void RestartGame()
+    {
+        restartButton.gameObject.SetActive(false);
+        StartNewRound();
     }
 }
+
--- Assets/Scripts/lights.cs
+++ Assets/Scripts/lights.cs
@@ -1,21 +1,55 @@
-using UnityEngine;
 
-public class lights : MonoBehaviour
+using UnityEngine;
+using System.Collections;
+using UnityEngine.InputSystem;
+
+public class Lights : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    
-    Random random = new Random();
-    int randomNumber = random.Next(1, 6);
-    
-    
-    void Start()
+    public int lightIndex;
+
+    private Collider2D objectCollider;
+    SpriteRenderer sr;
+    Color originalColor;
+
+    void Awake()
     {
-   
+        objectCollider = GetComponent<Collider2D>();
+        sr = GetComponent<SpriteRenderer>();
+        originalColor = sr.color;
     }
 
-    // Update is called once per frame
+    public IEnumerator Flash(float duration)
+    {
+        sr.color = Color.yellow;
+        yield return new WaitForSeconds(duration);
+        sr.color = originalColor;
+    }
+
+    public IEnumerator FlashRed(float duration)
+    {
+        sr.color = Color.red;
+        yield return new WaitForSeconds(duration);
+        sr.color = originalColor;
+    }
+
     void Update()
     {
-        
+        if (!Mouse.current.leftButton.wasPressedThisFrame)
+            return;
+
+        Vector2 mouseScreenPos = Mouse.current.position.ReadValue();
+        Vector3 worldPos = Camera.main.ScreenToWorldPoint(
+            new Vector3(mouseScreenPos.x, mouseScreenPos.y, Camera.main.nearClipPlane)
+        );
+
+        Vector2 worldPos2D = new Vector2(worldPos.x, worldPos.y);
+        Collider2D hit = Physics2D.OverlapPoint(worldPos2D);
+
+        if (hit != null && hit == objectCollider)
+        {
+            GameManager gm = FindFirstObjectByType<GameManager>();
+            if (gm != null)
+                gm.RegisterPlayerInput(lightIndex);
+        }
     }
 }
