9a1aa198385cc20aaa9806db5f6ac63f7ba5edb1
Jared Sinai Hernandez <48105264+jaredsina@users.noreply.github.com>
2026-03-17 17:11:23 -0400

    Changes to player script

--- Assets/orbCollect.cs
+++ Assets/orbCollect.cs
@@ -1,15 +1,25 @@
 using UnityEngine;
 
 public class orbCollect : MonoBehaviour
+
 {
     public int orbsCollected = 0;
+    
+    public GameObject redOrb;
+    public GameObject orangeOrb;
+    public GameObject yellowOrb;
+    public GameObject greenOrb;
+    public GameObject brownOrb;
+    public GameObject blueOrb;
+    public GameObject purpleOrb;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+    // Update is called once per frames
     void Update()
     {
         
@@ -17,8 +27,16 @@ public class orbCollect : MonoBehaviour
 
     void OnCollisionEnter2D(Collision2D collision)
     {
-        orbsCollected += 1;
+        if (collision.gameObject.name == "red")
+        {
+            Debug.Log("red picked up");
+            orbsCollected += 1;
+            redOrb.transform.position = new Vector2(7.68f, 4.45f);
+        }
+        
         Debug.Log(orbsCollected);
-        Destroy(gameObject);
+       
+        
+
     }
 }
