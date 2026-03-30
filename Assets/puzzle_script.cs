2c5b81df4980ebf4ba240c24ca8ead47ac1786ab
kris-3579 <52053880+kris-3579@users.noreply.github.com>
2026-03-30 17:50:12 -0400

    Update puzzle_script.cs

--- Assets/puzzle_script.cs
+++ Assets/puzzle_script.cs
@@ -1,37 +1,21 @@
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
-//detect if mouse cursor is touching shape
-//detect if mouse is clicked
-//change shape position to mouse position while clicked 
-//detect when mouse cursor is lifted
 public class puzzle_script : MonoBehaviour
 {
-    private bool isDraggable;
-
     private bool isDragging;
-
-    private Collider2D objectCollider;
-    
-    static puzzle_script currentlyDragging;
-
-    private start sx;
     private Vector3 offset;
+    private Camera mainCamera;
+
     void Start()
     {
-        objectCollider = GetComponent<Collider2D>();
-        isDraggable = false;
+        mainCamera = Camera.main;
         isDragging = false;
-        sx = new start();
-        sx.init();
-        
     }
 
     void OnMouseDown()
     {
         offset = transform.position - GetMouseWorldPosition();
-        isDraggable = true;
+        isDragging = true;
     }
 
     void OnMouseDrag()
@@ -49,55 +33,8 @@ public class puzzle_script : MonoBehaviour
 
     private Vector3 GetMouseWorldPosition()
     {
-        Vector3 
-    }
-    // Update is called once per frame 
-    
-
-    /*void dragAndDrop()
-    {
-        Vector3 mousePosition;
-        mousePosition = Mouse.current.position.ReadValue();
-        Vector3 worldPos3 = Camera.main.ScreenToWorldPoint(new Vector3(mousePosition.x, mousePosition.y, 0));
-        worldPos3.z = transform.position.z;
-        Vector2 worldPos2 = new Vector2(worldPos3.x, worldPos3.y);
-        Collider2D hit = Physics2D.OverlapPoint(worldPos2);
-        if (Mouse.current.leftButton.wasPressedThisFrame)
-        {
-<<<<<<< Updated upstream
-=======
-            //Debug.Log("puzzle script hit");
->>>>>>> Stashed changes
-            if (currentlyDragging != null)
-            {
-                return;
-            }
-            if (hit != null && objectCollider == hit)
-            {
-                isDraggable = true;
-                isDragging = true;
-                currentlyDragging = this;
-            }
-        }
-        
-        if (isDragging && currentlyDragging == this)
-        {
-            objectCollider.transform.position = worldPos3;
-            
-        }
-
-        if (Mouse.current.leftButton.wasReleasedThisFrame)
-        {
-            if (currentlyDragging == this)
-            {
-                currentlyDragging = null;
-            }
-            isDraggable = false;
-            isDragging = false;
-        }
-}*/
-    void Update()
-    {
-        dragAndDrop();
+        Vector3 mousePos = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+        mousePos.z = 0f;
+        return mousePos;
     }
 }
