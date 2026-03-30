cfb6c8f7bfc6db13d3b61038da2d9e5ef8cfdeae
kris-3579 <52053880+kris-3579@users.noreply.github.com>
2026-03-30 17:51:01 -0400

    Update start.cs

--- Assets/start.cs
+++ Assets/start.cs
@@ -1,67 +1,39 @@
-using UnityEngine;
-using System.Collections.Generic;
-public class start
+public class start : MonoBehaviour
 {
     public Dictionary<string, (float x, float y)> dictionary = new Dictionary<string, (float x, float y)>();
+    private SpriteRenderer[] renderers;
 
-    private GameObject background;
-    private GameObject mainbackground;
-    public void init()
+    void Start()
     {
-        background = GameObject.Find("background");
-        mainbackground = GameObject.Find("Canvas");
+        renderers = FindObjectsByType<SpriteRenderer>(FindObjectsSortMode.None);
         LogAllSpritePositions();
-        random_spot();
-        //Debug.Log(dictionary);
+        RandomSpot();
     }
 
     void LogAllSpritePositions()
     {
-        SpriteRenderer[]  renderers = Object.FindObjectsByType<SpriteRenderer>(FindObjectsSortMode.None);
         foreach (SpriteRenderer r in renderers)
         {
             if (r.name != "background")
             {
-                //GameObject go = r.gameObject;
-                //Vector3 worldpos = go.transform.position;
-                //Vector3 screenpos = Camera.main.WorldToScreenPoint(worldpos);
-                Vector3 centerposworld = r.bounds.center;
-                Vector3 centerposscreen = Camera.main.ScreenToWorldPoint(centerposworld);
-                dictionary.Add(r.name, (centerposscreen.x, centerposscreen.y));
+                Vector3 center = r.bounds.center;
+                dictionary.Add(r.name, (center.x, center.y));
                 r.enabled = false;
             }
-           
-            
         }
     }
 
-    void random_spot()
+    void RandomSpot()
     {
-        SpriteRenderer bgbox = background.GetComponent<SpriteRenderer>();
-        SpriteRenderer backgroundrenderer = mainbackground.GetComponent<SpriteRenderer>();
-        BoxCollider2D boxcollider = bgbox.GetComponent<BoxCollider2D>();
-        GameObject[] go = Object.FindObjectsByType<GameObject>(FindObjectsSortMode.None);
-        foreach (GameObject g in go)
+        foreach (SpriteRenderer r in renderers)
         {
-            if (g.name != "background" && g.name != "mainbackground" && g.name != "Main Camera" && g.name != "Canvas" && g.name != "Image" && g.name != "EventSystem" && g.name != "puzzle_script" && g.name != "[Debug Updater]")
+            if (r.name != "background")
             {
-                BoxCollider2D bbox =g.GetComponent<BoxCollider2D>();
-                float randomx = Random.Range(-2* backgroundrenderer.bounds.min.x, 2* backgroundrenderer.bounds.max.x);
-                float randomy = Random.Range(-2* backgroundrenderer.bounds.min.y, 2*backgroundrenderer.bounds.max.y);
-                g.transform.position = new Vector3(randomx, randomy, 0);
-                
-                /*
-                while (boxcollider.bounds.Intersects(bbox.bounds))
-                {
-                    randomx = Random.Range(backgroundrenderer.bounds.min.x, backgroundrenderer.bounds.max.x);
-                    randomy = Random.Range(backgroundrenderer.bounds.min.y, backgroundrenderer.bounds.max.y);
-                    g.transform.position = new Vector3(randomx, randomy, 0);
-                }*/
-                SpriteRenderer renderer = g.GetComponent<SpriteRenderer>();
-                renderer.enabled = true;
+                float randomX = Random.Range(-7f, 7f);
+                float randomY = Random.Range(-5f, 5f);
+                r.transform.position = new Vector3(randomX, randomY, 0);
+                r.enabled = true;
             }
-
         }
     }
-    
 }
